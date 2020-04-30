acc4f28d42e6f9ae6d86171f9668ef02935309d6
Camilo Vega <camilovegag@unisabana.edu.co>
2020-04-30 01:04:11 -0500

    Updates

---
+++ precache-manifest.cee96b3a658523967d612e961b92c552.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5377af8a560b4b07f3449c5691667d5e",
+    "url": "/tus-webpage/index.html"
+  },
+  {
+    "revision": "b7c8e2880f87fecdd425",
+    "url": "/tus-webpage/static/css/main.7a9a6899.chunk.css"
+  },
+  {
+    "revision": "b301ebf6f674d96ab295",
+    "url": "/tus-webpage/static/js/2.e2e45b41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tus-webpage/static/js/2.e2e45b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c8e2880f87fecdd425",
+    "url": "/tus-webpage/static/js/main.443b85d6.chunk.js"
+  },
+  {
+    "revision": "b6d65311a2290025f2d5",
+    "url": "/tus-webpage/static/js/runtime-main.72e3876b.js"
+  },
+  {
+    "revision": "aefb77e7c7c6af8fe6182745fec26fce",
+    "url": "/tus-webpage/static/media/barrica.aefb77e7.png"
+  },
+  {
+    "revision": "1cb3141146af68d88d114efd089d0b32",
+    "url": "/tus-webpage/static/media/beatlejuice.1cb31411.png"
+  },
+  {
+    "revision": "934340c3ce6b5837d7a5e818b36babff",
+    "url": "/tus-webpage/static/media/betun.934340c3.png"
+  },
+  {
+    "revision": "73196142c5896014bd28f6afc87726da",
+    "url": "/tus-webpage/static/media/bg-escuchar.73196142.png"
+  },
+  {
+    "revision": "f7219b6a45a2f053be388a23e800e133",
+    "url": "/tus-webpage/static/media/bg-header-contacto.f7219b6a.png"
+  },
+  {
+    "revision": "3b6cae124caf2fbe7ff51e68db2e9cd1",
+    "url": "/tus-webpage/static/media/bg-header-galeria.3b6cae12.png"
+  },
+  {
+    "revision": "cc6f0d8cd4e2b9404dae8b09b0dc104b",
+    "url": "/tus-webpage/static/media/bg-header-integrantes.cc6f0d8c.png"
+  },
+  {
+    "revision": "508154ae1b98b513f2429fbb10000af9",
+    "url": "/tus-webpage/static/media/bg-header-musica.508154ae.png"
+  },
+  {
+    "revision": "13032da194db7935bb859c08fbe1c83a",
+    "url": "/tus-webpage/static/media/bg-historia.13032da1.png"
+  },
+  {
+    "revision": "74cf29d739e7b9aaf58bca68fd1c0344",
+    "url": "/tus-webpage/static/media/bg-portada.74cf29d7.png"
+  },
+  {
+    "revision": "5fb4e9a45846f08db9b95493b23af4a9",
+    "url": "/tus-webpage/static/media/boyaqui.5fb4e9a4.png"
+  },
+  {
+    "revision": "9bda7d28e064705471768b58768ec676",
+    "url": "/tus-webpage/static/media/chorri.9bda7d28.png"
+  },
+  {
+    "revision": "da6b6320fb765c1cdd2685a48c8b4fdb",
+    "url": "/tus-webpage/static/media/jhonson.da6b6320.png"
+  },
+  {
+    "revision": "5d3ab7d77b11aaf3621b87077cf13455",
+    "url": "/tus-webpage/static/media/junior.5d3ab7d7.png"
+  },
+  {
+    "revision": "90ab6ea11157f670945cc5e29a9f9701",
+    "url": "/tus-webpage/static/media/locomia.90ab6ea1.png"
+  },
+  {
+    "revision": "07aafd50ec834434683bc707e40d77b5",
+    "url": "/tus-webpage/static/media/logo.07aafd50.png"
+  },
+  {
+    "revision": "9b9aac67ef337b31fa45d12d444ebfad",
+    "url": "/tus-webpage/static/media/pinzas.9b9aac67.png"
+  },
+  {
+    "revision": "993c741cfb659412b67ee1ef0e47afea",
+    "url": "/tus-webpage/static/media/up.993c741c.png"
+  },
+  {
+    "revision": "4205147a6f00248a4477c5b8d23e6af5",
+    "url": "/tus-webpage/static/media/wolowitz.4205147a.png"
+  }
+]);
